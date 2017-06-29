16e8e0d8553ca9c3ec7a7145be859efb777b90c2
hankHeish <minghan18@gmail.com>
2017-06-29 14:42:24 +0800

    Add files via upload

--- BKI Portfolio.R
+++ BKI Portfolio.R
@@ -1,36 +1,34 @@
 library(xts)
-library(quantmod)
-library(TTR)
-library(fPortfolio)
+library(zoo)
 
-histPrice <- read.csv("C:/Users/J1060019/Desktop/hist price.csv", header = TRUE)
-histPrice <- histPrice[, -3]
+#read Stock historical price
+#Stock <- read.csv("/Users/Heishminghan/Desktop/hist price.csv", header = TRUE, fileEncoding = 'big5')
+Stock <- read.csv("C:/Users/J1060019/Desktop/hist price.csv", header = T)
+Stock <- Stock[, -3]
+names(Stock) <- c("Date", "Code", "Clo.Price")
 
-names(histPrice) <- c("Date", "Code", "Clo.Price")
+code = c("2801", "2809", "2812", "2816", "2820", "2823", "2832", "2834", "2836", "2838", "2845"
+         , "2849", "2850", "2851", "2852", "2855", "2856", "2867", "2880", "2881", "2882", "2883"
+         , "2884", "2885", "2886", "2887", "2888", "2889", "2890", "2891", "2892", "5880", "6005")
 
-code <- c("2801", "2809", "2812", "2816", "2820", "2823", "2832", "2834", "2836", "2838", "2845"
-          , "2849", "2850", "2851", "2852", "2855", "2856", "2867", "2880", "2881", "2882", "2883"
-          , "2884", "2885", "2886", "2887", "2888", "2889", "2890", "2891", "2892", "5880", "6005")
-
-n <- dim(histPrice)[1]
-
-for (i in 2:n)
-{
-    if (histPrice[i, 2] == lag(histPrice[i - 1, 2]))
-        histPrice[i, 4] <- lag(histPrice[i - 1, 3])
-}
-
-histPrice[, 5] <- (histPrice[, 3] - histPrice[, 4]) / histPrice[, 4]
-histPrice <- na.omit(histPrice)
-names(histPrice) <- c("Date", "Code", "Clo.Price", "LagPrice", "DailyRet")
-histPrice <- histPrice[, -c(3, 4)]
-
-Ret2881 <- histPrice[histPrice$Code == "2881", ]
-Ret2881 <- xts(Ret2881, order.by = as.Date(Ret2881$Date))[, -c(1, 2)]
-
+StockList <- list()
 for (i in 1:length(code))
 {
-    as.data.frame(paste("Ret", code[i], sep = "")) <- histPrice[histPrice$Code == code[i], ]
+    StockList[[code[i]]] = Stock[Stock$Code == code[i], ]
+    StockList[[code[i]]] = xts(StockList[[code[i]]], order.by = as.Date(StockList[[code[i]]]$Date))
+    StockList[[code[i]]] <- StockList[[code[i]]][, -c(1)]
+    
+    StockList[[code[i]]]$LagPrice <- lag(StockList[[code[i]]]$Clo.Price)
+    StockList[[code[i]]] <- StockList[[code[i]]][-c(1), ]
+    StockList[[code[i]]]$Return <- (as.numeric(StockList[[code[i]]]$Clo.Price) - as.numeric(StockList[[code[i]]]$LagPrice)) / as.numeric(StockList[[code[i]]]$LagPrice)
 }
 
-#https://stackoverflow.com/questions/43350988/target-of-assignment-expands-to-non-language-object
+BKIRet <- matrix(0, length(StockList[[code[1]]]$Clo.Price), length(code))
+colnames(BKIRet) <- code
+
+for (i in 1:length(code))
+    BKIRet[, i] <- StockList[[code[i]]]$Return
+
+pairs(data.frame(BKIRet[, 19:29]), pch = 21, col = "darkgreen", main = "Correlations among 10 stocks return")
+
+Corr <- cor(BKIRet)
