0a2b8ad25b96035a7a2f39890e9868e49ddeae5e
hankHeish <minghan18@gmail.com>
2017-07-20 16:19:22 +0800

    Add files via upload

---
+++ R Crawler.R
@@ -0,0 +1,69 @@
+#R Crawler
+library(rvest)
+library(tmcn)
+
+#IMDB
+html <- read_html("http://www.imdb.com/title/tt1490017/")
+cast <- html_nodes(html, "span.itemprop")
+html_text(cast)
+
+cast <- html_nodes(html, "#titleCast .itemprop")
+html_text(cast)
+
+cast <- html_nodes(html, "#titleCast span.itemprop")
+html_text(cast)
+
+lego.movie <- read_html("http://www.imdb.com/title/tt1490017/")
+rating <- html_nodes(lego.movie, "strong span") %>% html_text()
+# html_text(rating)
+
+poster <- lego.movie %>%
+          html_nodes(".poster img") %>%
+          html_attr("src")
+
+#Liberty Times News
+html <- read_html("http://news.ltn.com.tw/list/BreakingNews")
+news.title <- html_nodes(html, ".tit")
+news.title.utf8 <- toUTF8(html_text(news.title))
+title.href <- html_attr(news.title, "href")
+my.news <- data.frame(title = news.title.utf8, href = title.href, stringsAsFactors = FALSE)
+
+
+#Apple Daily News
+ApplyDaily <- "http://www.appledaily.com.tw/"
+url.main <- paste0(ApplyDaily, "realtimenews/section/new/")
+apple.news <- read_html(url.main)
+
+news.time <- html_text(html_nodes(apple.news, '.rtddt time'))
+news.title <- html_text(html_nodes(apple.news, '.rtddt h1'))
+news.category <- html_text(html_nodes(apple.news, '.rtddt h2'))
+news.url <- html_attr(html_nodes(apple.news, '.rtddt a'), 'href')
+
+realtimenews <- data.frame(time = news.title, title = news.title, category = news.category, 
+                           url = paste0(ApplyDaily, news.url))
+
+getContent <- function(x){
+    tag <- html_nodes(read_html(x), '.trans')
+    text <- html_text(tag)
+    text
+}
+getContent(as.character(realtimenews$url[1]))
+
+#PTT R-Language
+url.main <- 'https://www.ptt.cc/bbs/R_Language/index.html'
+href.title <- html_nodes(read_html(url.main), ".title a")
+R.hrefs <- html_attr(href.title, 'href')
+
+R.article.data <- c()
+for (i in 1:length(R.hrefs))
+{
+    article.url <- paste0('https://www.ptt.cc/', R.hrefs[i])
+    article <- html_nodes(read_html(article.url), "#main-content")
+    article.content <- html_text(article)
+    article.utf8 <- iconv(article.content, 'utf8')
+    R.article.data <- c(R.article.data, article.utf8)
+    
+    Sys.sleep(sample(3:5, 1))
+}
+
+
