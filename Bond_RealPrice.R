c6f0d5ff762ce0ec2a89fcc93d9460523ca4948d
hankHeish <minghan18@gmail.com>
2017-09-01 16:42:06 +0800

    Add files via upload

--- Bond_RealPrice.R
+++ Bond_RealPrice.R
@@ -11,6 +11,13 @@ url.path <-  "http://www.tpex.org.tw"
 DL.csv.path <- "C:/Users/J1060019/Desktop/R"
 myCon_41 <- odbcConnect(dsn = 'cmoney_41', uid = 'hank', pwd = 'hank')
 
+sDate <- substr(Sys.time(), 1, 10)
+sTime <- gsub(":", "", substr(Sys.time(), 12, 20))
+if (nchar(sTime) == 5){
+    sTime <- paste0("0", sTime)
+}
+
+
 # 1.公債等值買賣斷即時行情-盤中報價行情表
 url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/ebts_ops/quotes.php?l=zh-tw")
 
@@ -35,17 +42,45 @@ Insert.Bond <- function(data, myCon){
     if(grepl("-", as.character(data[4]))){
         data[4] <- "NULL"
     }
-    sDate <- substr(Sys.time(), 1, 10)
-    sTime <- gsub(":", "", substr(Sys.time(), 12, 20))
     
-    mySQL <- "insert into [TestSherlock].[dbo].[Bond] values("
-    mySQL <- paste(mySQL, sDate, ", ", sTime, ", ", sep = "")
+    mySQL <- "insert into [TestSherlock].[dbo].[Bond] values('"
+    mySQL <- paste(mySQL, sDate, "', '", sTime, "', ", sep = "")
     mySQL <- paste(mySQL, "'", data[1], "', '", data[2], "', ",sep = "")
     mySQL <- paste(mySQL, paste0(data[3:4], collapse = ", "), ")", sep = "")
 
+    # cat(mySQL, "\n")
     sqlQuery(myCon, mySQL)
 }
 apply(Bond, 1, FUN = Insert.Bond, myCon_41)
+
+# 1.2 公債等值買賣斷即時行情-成交行情揭示
+url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/ebts_ops/trade.php?l=zh-tw")
+
+Bond.csv <- url %>% 
+    html_nodes(".btn-csv") %>%
+    html_attr('href')
+
+csv.path <- paste0(url.path, Bond.csv)
+download.file(csv.path, destfile = "Bond_trade.csv", method = "libcurl")
+
+Bond_trade <- read.csv(paste0(DL.csv.path, "/Bond_trade.csv"), header = F)
+Bond_trade <- Bond_trade[-c(1:5), ]
+colnames(Bond_trade) <- c("Bond_Code", "Bond_Name", "Yield", "yield_change", "max_yield", 
+                          "min_yield", "CumTrade", "ref_yield")
+
+Insert.Bond_trade <- function(data, myCon){
+    if (substr(data[1], 1, 1) == 'A'){
+        
+        mySQL <- "insert into [TestSherlock].[dbo].[Bond_trade] values('"
+        mySQL <- paste0(mySQL, sDate, "', '", sTime, "', ")
+        mySQL <- paste0(mySQL, "'", data[1], "', '", data[2], "', ", paste0(data[3:8], collapse = ","), ")")
+
+        sqlQuery(myCon, mySQL)
+    }
+    
+}
+apply(Bond_trade, 1, FUN = Insert.Bond_trade, myCon_41)
+
 
 # 2.公債等殖附條件即時行情-盤中報價行情表
 url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/ebts_rps/quotes.php?l=zh-tw")
@@ -55,9 +90,9 @@ Bond_RPRS.csv <- url %>%
     html_attr('href')
 
 RPRS_csv.path <- paste0(url.path, Bond_RPRS.csv)
-download.file(RPRS_csv.path, destfile = "Bond_RPRS.csv", method = "libcurl")
+download.file(RPRS_csv.path, destfile = "Bond_RPRS_quotes.csv", method = "libcurl")
 
-Bond_RPRS <- read.csv(paste0(DL.csv.path, "/Bond_RPRS.csv"), header = F)
+Bond_RPRS <- read.csv(paste0(DL.csv.path, "/Bond_RPRS_quotes.csv"), header = F)
 
 len <- dim(Bond_RPRS)[2]
 for (i in 1:len){
@@ -76,11 +111,9 @@ if (dim(Bond_RPRS)[1] > 0){
     Insert.Bond_RPRS <- function(data, myCon){
     
         insert.data <- gsub("-", "NULL", data)
-        sDate <- substr(Sys.time(), 1, 10)
-        sTime <- gsub(":", "", substr(Sys.time(), 12, 20))
         
-        mySQL <- "insert into [TestSherlock].[dbo].[Bond_RPRS] values("
-        mySQL <- paste0(mySQL, sDate, ", ", sTime, ", ")
+        mySQL <- "insert into [TestSherlock].[dbo].[Bond_RPRS] values('"
+        mySQL <- paste0(mySQL, sDate, "', '", sTime, "', ")
         mySQL <- paste0(mySQL, "'", insert.data[1], "', '", insert.data[2], "', ")
         mySQL <- paste0(mySQL, paste0(insert.data[3:20], collapse = ", "), ")")
         
@@ -89,8 +122,44 @@ if (dim(Bond_RPRS)[1] > 0){
     apply(Bond_RPRS, 1, FUN = Insert.Bond_RPRS, myCon_41)
 }
 
+# 2.2 
+url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/ebts_rps/trade.php?l=zh-tw")
 
-# 3.公司債即時行情- a.等殖買賣斷盤中報價行情
+Bond_RPRS.csv <- url %>%
+    html_nodes(".btn-csv") %>%
+    html_attr('href')
+
+RPRS_csv.path <- paste0(url.path, Bond_RPRS.csv)
+download.file(RPRS_csv.path, destfile = "Bond_RPRS_trade.csv", method = "libcurl")
+
+Bond_RPRS_trade <- read.csv(paste0(DL.csv.path, "/Bond_RPRS_trade.csv"), header = F)
+
+len <- dim(Bond_RPRS_trade)[2]
+for (i in 1:len){
+    if (i == 1){
+        RPRS_trade <- as.character(Bond_RPRS_trade[3, i])
+    }else{
+        RPRS_trade <- cbind(RPRS_trade, as.character(Bond_RPRS_trade[3, i]))
+    }
+}
+Bond_RPRS_trade <- Bond_RPRS_trade[-c(1:3), ]
+colnames(Bond_RPRS_trade) <- RPRS_trade
+
+Insert.Bond_RPRS_trade <- function(data, myCon){
+    temp <- gsub("-", "NULL", data)
+    
+    mySQL <- "insert into [TestSherlock].[dbo].[Bond_RPRS_trade] values('"
+    mySQL <- paste0(mySQL, sDate, "', '", sTime, "', ")
+    mySQL <- paste0(mySQL, "'", temp[1], "', '", temp[2], "', ", paste0(temp[3:33], collapse = ","), ")")
+    
+    sqlQuery(myCon, mySQL)
+}
+
+if (dim(Bond_RPRS_trade)[2] > 0){
+    apply(Bond_RPRS_trade, 1, Insert.Bond_RPRS_trade, myCon_41)
+}
+
+# 3.公司債即時行情- a1.等殖買賣斷盤中報價行情
 url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/cb/quotes.php?l=zh-tw")
 
 Bond_CB.csv <- url %>%
@@ -123,11 +192,9 @@ if (dim(Bond_CB)[1] != 0)
         if (data[4] == "-"){
             data[4] <- "NULL"
         }
-        sDate <- substr(Sys.time(), 1, 10)
-        sTime <- gsub(":", "", substr(Sys.time(), 12, 20))
         
-        mySQL <- "insert into [TestSherlock].[dbo].[Bond_CB] values("
-        mySQL <- paste0(mySQL, sDate, ", ", sTime, ", ")
+        mySQL <- "insert into [TestSherlock].[dbo].[Bond_CB] values('"
+        mySQL <- paste0(mySQL, sDate, "', '", sTime, "', ")
         mySQL <- paste(mySQL, "'", data[1], "', '", data[2], "', ",sep = "")
         mySQL <- paste(mySQL, paste0(data[3:4], collapse = ", "), ")", sep = "")
         
@@ -136,7 +203,43 @@ if (dim(Bond_CB)[1] != 0)
     apply(Bond_CB, 1, FUN = Insert.Bond_CB, myCon_41)
 }
 
-# 3.公司債即時行情- b.處所議價盤中報價行情
+# 3.公司債即時行情- a2.成交行情揭示
+url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/cb/trade.php?l=zh-tw")
+
+Bond_CB.csv <- url %>%
+    html_nodes(".btn-csv") %>%
+    html_attr('href')
+
+CB_csv.path <- paste0(url.path, Bond_CB.csv)
+download.file(CB_csv.path, destfile = "Bond_CB_trade.csv", method = "libcurl")
+
+Bond_CB_trade <- read.csv(paste0(DL.csv.path, "/Bond_CB_trade.csv"), header = F)
+
+len <- dim(Bond_CB_trade)[2]
+for (i in 1:len){
+    if (i == 1){
+        CB_trade_name <- as.character(Bond_CB_trade[3, i]) 
+    }else{
+        CB_trade_name <- cbind(CB_trade_name, as.character(Bond_CB_trade[3, i]))
+    }
+}
+Bond_CB_trade <- Bond_CB_trade[-c(1:3), ]
+colnames(Bond_CB_trade) <- CB_trade_name
+
+insert.Bond_CB_trade <- function(data, myCon){
+    if (substr(data[1], 1, 1) == 'A'){
+        
+        mySQL <- "insert into [TestSherlock].[dbo].[Bond_CB_trade] values('"
+        mySQL <- paste0(mySQL, sDate, "', '", sTime, "', ")
+        mySQL <- paste0(mySQL, "'", data[1], "', '", data[2], "', ", paste0(data[3:8], collapse = ","), ")")
+        
+        sqlQuery(myCon, mySQL)
+    }
+    
+}
+apply(Bond_CB_trade, 1, FUN = insert.Bond_CB_trade, myCon_41)
+
+# 3.公司債即時行情- b1.處所議價盤中報價行情
 url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/fixed_income/WAW30601.php?l=zh-tw")
 
 Bond_FX.csv <- url %>% 
@@ -165,13 +268,11 @@ if (dim(Bond_FX)[1] != 0){
     
     Insert.Bond_FX <- function(data, myCon){
         insert.data <- gsub("-", "NULL", data)
-        sDate <- substr(Sys.time(), 1, 10)
-        sTime <- gsub(":", "", substr(Sys.time(), 12, 20))
         
-        mySQL <- "insert into [TestSherlock].[dbo].[Bond_FX] values("
-        mySQL <- paste0(mySQL, sDate, ", ", sTime, ", ")
+        mySQL <- "insert into [TestSherlock].[dbo].[Bond_FX] values('"
+        mySQL <- paste0(mySQL, sDate, "', '", sTime, "', ")
         mySQL <- paste(mySQL, "'", insert.data[1], "', '", insert.data[2], "', ",sep = "")
-        mySQL <- paste(mySQL, paste0(as.character(insert.data[3:4]), collapse = ","), ", ", sep = "")
+        mySQL <- paste(mySQL, "'", insert.data[3], "', '", insert.data[4], "', ", sep = "")
         mySQL <- paste(mySQL, paste0(insert.data[5:9], collapse = ", "), ")", sep = "")
         
         # print(mySQL)
@@ -180,6 +281,49 @@ if (dim(Bond_FX)[1] != 0){
     
     apply(Bond_FX, 1, FUN = Insert.Bond_FX, myCon_41)
 }
+
+# 3.公司債即時行情- b2.處所議價盤中成交行情
+url <- read_html("http://www.tpex.org.tw/web/bond/tradeinfo/market/fixed_income/trade_tran07.php?l=zh-tw")
+
+Bond_FX.csv <- url %>% 
+    html_nodes(".btn-csv") %>%
+    html_attr('href')
+
+FX_csv.path <- paste0(url.path, Bond_FX.csv)
+download.file(FX_csv.path, destfile = "Bond_FX_trade.csv", method = "libcurl")
+
+Bond_FX_trade <- read.csv(paste0(DL.csv.path, "/Bond_FX_trade.csv"), header = F)
+
+len <- dim(Bond_FX_trade)[2]
+for (i in 1:len){
+    if (i == 1){
+        FX_trade_name <- as.character(Bond_FX_trade[3, i])
+    }else{
+        FX_trade_name <- cbind(FX_trade_name, as.character(Bond_FX_trade[3, i]))
+    }
+}
+
+colnames(Bond_FX_trade) <- FX_trade_name
+Bond_FX_trade <- Bond_FX_trade[-c(1:3), ]
+
+Insert.Bond_FX_trade <- function(data, myCon){
+    insert.data <- gsub("-", "NULL", data)
+    
+    mySQL <- "insert into [TestSherlock].[dbo].[Bond_FX_trade] values('"
+    mySQL <- paste0(mySQL, sDate, "', '", sTime, "', ")
+    mySQL <- paste(mySQL, "'", data[1], "', '", data[2], "', '", data[3], "', ")
+    mySQL <- paste(mySQL, "'", data[4], "', '", data[5], "', ", sep = "")
+    mySQL <- paste(mySQL, paste0(data[6:9], collapse = ", "), ")", sep = "")
+    
+    sqlQuery(myCon, mySQL)
+}
+if (dim(Bond_FX_trade)[2] > 0){
+    apply(Bond_FX_trade, 1, Insert.Bond_FX_trade, myCon_41)
+}
+
+
+
+
 
 
 
---
+++ Bond_Term_Structure.R
@@ -0,0 +1,192 @@
+# Term Strucutre - Bond
+library(stringr)
+library(RODBC)
+library(dplyr)
+
+# Connect to ODBC
+myCon_41 <- odbcConnect(dsn = 'cmoney_41', uid = 'hank', pwd = 'hank')
+
+# Select Last Trading Date
+MySQL <- "select top 2 Tradingdate"
+MySQL <- paste0(MySQL, " from [DBMain].[dbo].[Tradingdate]")
+MySQL <- paste0(MySQL, " where Tradingdate <= '", substr(Sys.Date(), 1, 10), "'")
+MySQL <- paste0(MySQL, " order by Tradingdate desc")
+
+LastTrdDate <- sqlQuery(myCon_41, MySQL)
+TrdDate <- c(substr(LastTrdDate[1, 1], 1, 10), substr(LastTrdDate[2, 1], 1, 10))
+
+# Select Convertible Bond
+MySQL <- "select 資料日期, 債券代號, 債券名稱, 市場利率, DV01"
+MySQL <- paste0(MySQL, " from [PL].[dbo].[債券部位檔]")
+MySQL <- paste0(MySQL, " where 資料日期 = '", LastTrdDate[2, 1], "'")
+
+CB <- sqlQuery(myCon_41, MySQL)
+colnames(CB) <- c("TxDate", "Bond_Code", "Bond_Name", "ir", "DV01")
+
+# Select Bond Due Date
+MySQL <- "select 年月, 名稱, 債券代碼, 發行日期, 到期日期"
+MySQL <- paste0(MySQL, " from [Cmoney].[dbo].[債券發行基本資料表]")
+MySQL <- paste0(MySQL, " where 到期日期 >= '", substr(Sys.Date(), 1, 10), "' and 債券代碼 is not NULL")
+MySQL <- paste0(MySQL, " order by 債券代碼, 年月 desc")
+
+BondDue <- sqlQuery(myCon_41, MySQL)
+BondDue <- BondDue %>%
+    distinct(BondDue[, 3], .keep_all = TRUE)
+n <- dim(BondDue)[2]
+BondDue <- BondDue[, -n]
+colnames(BondDue) <- c("TxDate", "Bond_Name", "Bond_Code", "Issue_Date", "Due_Date")
+
+# Merge Cb and BondDue
+CB <- merge(x = CB, y = BondDue, by = "Bond_Code", all.x = TRUE)
+CB <- CB %>%
+    mutate(Durat = CB[, 9] - CB[, 2]) %>%
+    arrange(Durat) %>%
+    select(Bond_Code:DV01, Durat)
+
+# Select Government Bond
+BondName <- c("sDate", "yDate")
+for (i in 1:length(TrdDate))
+{
+    LastTrdDate <- TrdDate[i]
+    
+    MySQL <- "select *"
+    MySQL <- paste0(MySQL, " from [TestSherlock].[dbo].[Bond]")
+    MySQL <- paste0(MySQL, " where TxDate = '", LastTrdDate, "' and [Bond Code] like 'A0610%'")
+    
+    GovBond <- sqlQuery(myCon_41, MySQL)
+    
+    GovBond <- GovBond %>%
+        arrange(desc(as.integer(TimeTag))) %>% 
+        distinct(GovBond[, 3], .keep_all = TRUE) 
+    n <- dim(GovBond)[2]
+    GovBond <- GovBond[, -n]
+    colnames(GovBond)[3:4] <- c("Bond_Code", "Bond_Name")
+    
+    # Merge GovBond and BonDue
+    GovBond <- merge(x = GovBond, y = BondDue, by = "Bond_Code", all.x = TRUE)
+    GovBond <- GovBond %>%
+        mutate(Durat = as.integer(GovBond[, 10] - GovBond[, 2])) %>%
+        arrange(Durat) %>%
+        select(Bond_Code:Ask, Durat)
+
+    temp <- paste0("GovBond_", BondName[i])
+    assign(temp, GovBond)
+}
+
+# Dispose NA Value in GovBond Duration
+GovCode <- c("A06107", "A06105", "A06102", "A06104", "A06108", "A06106")
+GovDue <- c(730, 1825, 1825, 3650, 7300, 10950)
+
+Fill_NULL_DueDate <- function(x){
+    if (is.na(x[7])){
+        for (i in 1:length(GovCode)){
+            if (x[1] == GovCode[i]){
+                x[7] <- GovDue[i]
+
+            }
+        }
+    }
+    
+    return (x)
+}
+
+GovBond_sDate <- apply(GovBond_sDate, 1, Fill_NULL_DueDate) %>%
+    t() %>% 
+    as.data.frame() %>%
+    arrange(as.numeric(levels(Durat))[Durat])
+GovBond_yDate <- apply(GovBond_yDate, 1, Fill_NULL_DueDate) %>%
+    t() %>% 
+    as.data.frame() %>%
+    arrange(as.numeric(levels(Durat))[Durat])
+
+GovBond_sDate[, 5:7] <- sapply(GovBond_sDate[, 5:7], function(x) as.numeric(as.character(x)))
+GovBond_yDate[, 5:7] <- sapply(GovBond_yDate[, 5:7], function(x) as.numeric(as.character(x)))
+
+# Plot Term Structure
+par(mfrow = c(1, 1))
+plot(GovBond_sDate$Durat, GovBond_sDate$Bid, type = "b", lwd = 1, 
+     xlab = "Days", ylab = "interest rate(%)", main = "Term Structure(Bid)")
+lines(GovBond_yDate$Durat, GovBond_yDate$Bid, type = "b", lty = 2, lwd = 1, col = "red")
+legend("bottomright", c("Today", "Yesterday"), lty = c(1, 2), lwd = 1, 
+       col = c("black", "red"), cex = 0.7)
+
+plot(GovBond_sDate$Durat, GovBond_sDate$Ask, type = "b", lwd = 1, 
+     xlab = "Days", ylab = "interest rate(%)", main = "Term Structure(Ask)")
+lines(GovBond_yDate$Durat, GovBond_yDate$Ask, type = "b", lty = 2, lwd = 1, col = "red")
+legend("bottomright", c("Today", "Yesterday"), lty = c(1, 2), lwd = 1, 
+       col = c("black", "red"), cex = 0.7)
+
+# Calculate Interest Rate Change 
+Chg.Rate.Bid <- cbind(GovBond_yDate$Bid, GovBond_sDate$Bid, GovBond_sDate$Durat)
+colnames(Chg.Rate.Bid) <- c("Bid_y", "Bid_s", "Durat")
+Chg.Rate.Ask <- cbind(GovBond_yDate$Ask, GovBond_sDate$Ask, GovBond_sDate$Durat)
+colnames(Chg.Rate.Ask) <- c("Ask_y", "Ask_s", "Durat")
+
+# Interpolation Method
+Interpolation <- function(x1, y1, x2, y2, x){
+    return(y1 + (x-x1)/(x2-x1) * (y2-y1))
+    
+}
+Find_Range <- function(x, Rate, i){
+    if (x < Rate[1, 3]){
+        Interpolation(0, 0.1, Rate[1, 3], Rate[1, i], x)
+       
+    }else if (Rate[1, 3] < x & x < Rate[2, 3]){
+        Interpolation(Rate[1, 3], Rate[1, i], Rate[2, 3], Rate[2, i], x)
+        
+    }else if (Rate[2, 3] < x & x < Rate[3, 3]){
+        Interpolation(Rate[2, 3], Rate[2, i], Rate[3, 3], Rate[3, i], x)
+        
+    }else if (Rate[3, 3] < x & x < Rate[4, 3]){
+        Interpolation(Rate[3, 3], Rate[3, i], Rate[4, 3], Rate[4, i], x)
+        
+    }else if (Rate[4, 3] < x & x < Rate[5, 3]){
+        Interpolation(Rate[4, 3], Rate[4, i], Rate[5, 3], Rate[5, i], x)
+        
+    }else if (Rate[5, 3] < x & x < Rate[6, 3]){
+        Interpolation(Rate[5, 3], Rate[5, i], Rate[6, 3], Rate[6, i], x)
+        
+    }else{
+        Interpolation(Rate[6, 3], Rate[6, i], 12775, 2, x)
+        
+    }
+    
+}
+# Check Missing Value in Bid, Ask
+# check_NA_value <- function(x){
+#     if (is.na(x)){
+#         
+#     }
+# }
+# 
+# sapply(Chg.Rate.Bid$Bid_y, check_NA_value)
+# 
+
+# Interest Rate Change bp
+CB$y.Bid.IR <- sapply(CB[, 6], Find_Range, Chg.Rate.Bid, 1)
+CB$s.Bid.IR <- sapply(CB[, 6], Find_Range, Chg.Rate.Bid, 2)
+CB$Bid.chg.bp <- (CB$s.Bid.IR - CB$y.Bid.IR) / CB$y.Bid.IR * 10000
+CB$DV01.Bid <- (1 + CB$Bid.chg.bp / 10000) * CB$DV01
+
+CB$y.Ask.IR <- sapply(CB[, 6], Find_Range, Chg.Rate.Ask, 1)
+CB$s.Ask.IR <- sapply(CB[, 6], Find_Range, Chg.Rate.Ask, 2)
+CB$Ask.chg.bp <- (CB$s.Ask.IR - CB$y.Ask.IR) / CB$y.Ask.IR * 10000
+CB$DV01.Ask <- (1 + CB$Ask.chg.bp / 10000) * CB$DV01
+
+# Insert Result to [TestSherlock].[dbo].[Instant_DV01.Bond]
+Insert_Instant_DV01 <- function(x, MyCon){
+    sDate <- substr(Sys.Date(), 1, 10)
+    sTime <- gsub(":", "", substr(Sys.time(), 12, 20))
+    
+    MySQL <- "Insert into [TestSherlock].[dbo].[Instant_DV01.Bond] values('"
+    MySQL <- paste0(MySQL, sDate, "', ", sTime, ", '", x[1], "', '", x[3], "', ")
+    MySQL <- paste0(MySQL, paste0(x[4:5], collapse = ", "), ", ", paste(x[7:14], collapse = ","), ")")
+    
+    sqlQuery(MyCon, MySQL)
+}
+apply(CB, 1, FUN = Insert_Instant_DV01, myCon_41)
+
+# CB <- CB[, -c(6:10)]
+
+# Close Connection to ODBC
+odbcClose(myCon_41)
