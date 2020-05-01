195463e8dafef352d90d9f8db9e5578505b54028
R <minghan18@gmail.com>
2020-05-01 21:43:36 +0800

    Delete ARMA Model.R

--- ARMA Model.R
+++
@@ -1,7 +0,0 @@
-#ARMA Model
-library(xts)
-
-CPI <- read.csv("C:/Users/J1060019/Desktop/R Quant/part 4/024/CPI.csv", header = TRUE)
-CPI <- xts(CPI[, -1], order.by = as.Date(CPI$time))
-
-CPITrain <- CPI[1:(length(CPI) - 3), ]
